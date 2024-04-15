83d4c9d03a4b790742a6ab0c12f3793692fbed03
Jason De Lorme <delormej@gmail.com>
2024-04-15 19:07:45 -0400

    Clean up, add parameter for temp.

--- Vertex/VertexAIModelGenerator.cs
+++ Vertex/VertexAIModelGenerator.cs
@@ -1,41 +1,38 @@
-using Google.Api.Gax.Grpc;
 using Google.Cloud.AIPlatform.V1;
 
 namespace GenerativeQuote;
 
 public class VertexAIModelGenerator
 {
-    private readonly HttpClient _httpClient;
     private readonly string _apiEndpoint;
-    private readonly string _projectId;
-    private readonly string _modelId;
-    private readonly string _locationId;
     private readonly string _model;
     private readonly GenerationConfig _generationConfig;
 
     public VertexAIModelGenerator(
         string projectId, 
         string modelId = "gemini-1.0-pro-001", 
-        string apiEndpoint = "us-central1-aiplatform.googleapis.com",         
-        string locationId = "us-central1")
+        string locationId = "us-central1",
+        float temperature = 0.6f)
     {
-        _projectId = projectId;
-        _modelId = modelId;
-        _locationId = locationId;
-        _apiEndpoint = $"{_locationId}-aiplatform.googleapis.com";
-
         // `projects/{project}/locations/{location}/publishers/*/models/*`
-        _model = $"projects/{_projectId}/locations/{_locationId}/publishers/google/models/{_modelId}";
+        _model = $"projects/{projectId}/locations/{locationId}/publishers/google/models/{modelId}";
+        
+        _apiEndpoint = $"{locationId}-aiplatform.googleapis.com";
 
         _generationConfig = new GenerationConfig() 
         { 
             CandidateCount = 1, 
             MaxOutputTokens = 256, 
-            Temperature = 0.6f, 
+            Temperature = temperature, 
             TopP = 1
         };
     }
 
+    /// <summary>
+    /// Invokes the Vertex AI Model to generate text.
+    /// </summary>
+    /// <param name="textPrompt"></param>
+    /// <returns></returns>
     public async Task<string> GenerateTextAsync(string textPrompt)
     {
         // Create a prediction service client.
@@ -47,7 +44,7 @@ public class VertexAIModelGenerator
         var content = new Content() { Role = "USER" };
         content.Parts.Add(new Part() { Text = textPrompt });
 
-        GenerateContentRequest request = new GenerateContentRequest
+        var request = new GenerateContentRequest
         {
             Contents = { content, },
             GenerationConfig = _generationConfig,
